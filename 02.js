4f3af778bf9dfd415cc69d30d8fb47ae3fb29514
10646028 <10646028@ntub.edu.tw>
2020-07-31 15:42:11 +0800

    02

---
+++ 02.js
@@ -0,0 +1,19 @@
+/*$('button.show').hide("slow");
+$('p.hide').click(function(){
+    $('p').hide();
+    $('button.show').show();
+})
+$('button.show').click(function(){
+    $('p').show();
+    $('button.show').hide();
+})*/
+
+$('button.show').hide();
+$('p.hide').click(function(){
+    $('p.hide').toggle();
+    $('button.show').toggle();
+})
+$('button.show').click(function(){
+    $('p.hide').toggle();
+    $('button.show').toggle();
+})
