2a5d50a6a9b2d5b134b4365fb20e830419bb11bc
10646028 <10646028@ntub.edu.tw>
2020-07-31 15:01:19 +0800

    01

---
+++ 01.js
@@ -0,0 +1,18 @@
+var price = 2000
+var fee = 100
+var discount = 0.5
+var total = price*discount+fee
+$('h1').text(total)
+// 變數total可動態改變，並寫入h1這個元素裡
+$('h2').text(total+50+"和1234")
+// 數字相加直接用"+"連接；文字相連用"""
+
+$('button').click(function(){
+    $('button').text("已經按了按鈕");
+})
+// 當button被按下，執行以下function：將button的文字改成「已經按了按鈕」
+
+$('p').click(function(){
+    $(this).hide();
+})
+// 當p被點擊時，便隱藏
