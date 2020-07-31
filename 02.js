5345081e13889d68ecdad8a6ce1386df4a9833f2
10646028 <10646028@ntub.edu.tw>
2020-07-31 16:05:10 +0800

    02

--- 02.js
+++ 02.js
@@ -1,3 +1,4 @@
+//===============隱藏與顯示===============
 /*$('button.show').hide("slow");
 $('p.hide').click(function(){
     $('p').hide();
@@ -17,3 +18,11 @@ $('button.show').click(function(){
     $('p.hide').toggle();
     $('button.show').toggle();
 })
+
+//===============淡入與淡出===============
+$('div').css('background-color','yellow');
+//將div上色，較容易觀察變化
+$('.title').click(function(){
+    $('.context').fadeToggle();
+})
+//當div.title被點擊時，div.context觸發fadeToggle();
