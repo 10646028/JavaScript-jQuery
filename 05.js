18688ab4415cbbd78bc49e1a50fb38121e426653
10646028 <10646028@ntub.edu.tw>
2020-08-01 16:39:53 +0800

    05

---
+++ 05.js
@@ -0,0 +1,32 @@
+let myVariable1 = 'puppy';
+var myVariable2 = 'kitty';
+const pi = 3.1415;
+//number
+var n1 = 212;
+var n2 = n1*pi; // 665.998
+console.log(n1);
+console.log(n2);
+console.log(typeof(n1));
+console.log(typeof(n2));
+//string
+var p = 'puppy';
+var k = 'kitty';
+console.log(p);
+console.log(k);
+console.log(typeof(p));
+console.log(typeof(k));
+//boolean 
+var a = true;
+var b = false;
+console.log(a);
+console.log(b);
+console.log(typeof(a));
+console.log(typeof(b));
+//undefined 
+var un;
+console.log(un);
+console.log(typeof(un));
+//null 
+var nu = null;
+console.log(nu);
+console.log(typeof(nu));
